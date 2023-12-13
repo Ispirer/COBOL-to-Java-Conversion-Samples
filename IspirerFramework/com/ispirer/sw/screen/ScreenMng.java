73c421ae47170ebd8bd9d0e87c30a792afef7e90
Ispirer Systems <142319792+Ispirer@users.noreply.github.com>
2023-12-13 18:45:53 +0300

    Add files via upload

---
+++ IspirerFramework/com/ispirer/sw/screen/ScreenMng.java
@@ -0,0 +1,56 @@
+/*
+	© 2021, Ispirer Systems OÜ. All rights reserved.
+	NOTICE OF LICENSE
+	This file\library is Ispirer Reusable Code (“IRC”) and you are granted a non-exclusive, worldwide, perpetual, irrevocable and fully paid up license to use, modify, adapt, sublicense and otherwise exploit this IRC as provided below and under the terms of Ispirer Systems OÜ. Reusable Code License Agreement (“License”), which can be found in supplementary LICENSE.txt file. By using this IRC, you acknowledge that you have read the License and agree with its terms as well as with the fact that IRC is the property of and belongs to Ispirer Systems OÜ only.
+	IF YOU ARE NOT AGREE WITH THE TERMS OF THE LICENSE, PLEASE, STOP USING THIS IRC IMMEDIATELY! 
+	PLEASE, NOTE, THAT IRC IS DISTRIBUTED “AS IS” AND WITHOUT ANY WARRANTY. IN NO EVENT WILL ISPIRER BE LIABLE FOR ANY DAMAGES, CLAIMS OR COSTS WHATSOEVER OR ANY CONSEQUENTIAL, INDIRECT, INCIDENTAL DAMAGES, OR ANY LOST PROFITS OR LOST SAVINGS. 
+	Redistributions of this IRC must retain the above copyright notice and a list of significant changes made to this IRC with indication of its author(s) and date of changes.
+	If you need more information, or you think that the License has been violated, please let us know by e-mail: legal.department@ispirer.com  
+*/
+package com.ispirer.sw.screen;
+
+import com.ispirer.sw.types.Form;
+import com.ispirer.sw.types.Program;
+
+import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ScreenMng {
+	public static JFrame form;
+
+	private ScreenMng() {
+		UIManager.put("TextField.caretForeground", new ColorUIResource(Color.green));
+		form.setName("FVBX01");
+		form.setBounds(268, 106, 900, 613);
+		form.setTitle("");
+		form.setLocationRelativeTo(null);
+		form.setLayout(null);
+		form.getContentPane().setBackground(Color.black);
+	}
+
+	public static void formvrto(Program program, Form form, Object data) {
+		form.initData(data.toString());
+		form.initActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				program.procedureDivision(form.output().toCharArray());
+				getForm().dispose();
+			}
+		});
+		form.initForm();
+		getForm().setVisible(true);
+	}
+
+	public static void clear() {
+		getForm().getContentPane().removeAll();
+		getForm().getContentPane().revalidate();
+		getForm().getContentPane().repaint();
+	}
+
+	public static JFrame getForm() {
+		return form;
+	}
+}
